347b5cd69e2180974ebc15a987f62020c12d8e09
venidhi <venidhi@google.com>
2026-04-06 08:44:46 +0530

    Fix: Revert erroneous plan check step in immutable backup update test

--- mmv1/third_party/terraform/services/netapp/resource_netapp_backup_test.go
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_backup_test.go
@@ -377,14 +377,9 @@ func TestAccNetappBackup_NetappImmutableBackup(t *testing.T) {
 				ImportStateVerify:       true,
 				ImportStateVerifyIgnore: []string{"labels", "location", "name", "terraform_labels", "vault_name"},
 			},
-			{
-				Config: testAccNetappBackup_CrossRegionBackupUpdate(context),
-				ConfigPlanChecks: resource.ConfigPlanChecks{
-					PreApply: []plancheck.PlanCheck{
-						plancheck.ExpectResourceAction("google_netapp_backup.test_backup", plancheck.ResourceActionUpdate),
-					},
-				},
-			},
+      {
+        Config: testAccNetappBackup_ImmutableBackupUpdate(context),
+      },
 			{
 				ResourceName:            "google_netapp_backup.test_backup",
 				ImportState:             true,
