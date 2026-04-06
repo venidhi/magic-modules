be4908c537cd279cfc4f5e7cde073f85cc069d1b
venidhi <venidhi@google.com>
2026-04-06 08:41:32 +0530

    Added plan check for CrossRegionBackupUpdate test

--- mmv1/third_party/terraform/services/netapp/resource_netapp_backup_test.go
+++ mmv1/third_party/terraform/services/netapp/resource_netapp_backup_test.go
@@ -378,7 +378,12 @@ func TestAccNetappBackup_NetappImmutableBackup(t *testing.T) {
 				ImportStateVerifyIgnore: []string{"labels", "location", "name", "terraform_labels", "vault_name"},
 			},
 			{
-				Config: testAccNetappBackup_ImmutableBackupUpdate(context),
+				Config: testAccNetappBackup_CrossRegionBackupUpdate(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_netapp_backup.test_backup", plancheck.ResourceActionUpdate),
+					},
+				},
 			},
 			{
 				ResourceName:            "google_netapp_backup.test_backup",
@@ -548,6 +553,11 @@ func TestAccNetappBackup_NetappCrossRegionBackup(t *testing.T) {
 			},
 			{
 				Config: testAccNetappBackup_CrossRegionBackupUpdate(context),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction("google_netapp_backup.test_backup", plancheck.ResourceActionUpdate),
+					},
+				},
 			},
 			{
 				ResourceName:            "google_netapp_backup.test_backup",
